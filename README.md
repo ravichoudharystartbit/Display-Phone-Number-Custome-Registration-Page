8447c08f85beaa15ea6d2952e85ab5b56ded3fb6
Ravi Choudhary <32158355+ravichoudharystartbit@users.noreply.github.com>
2023-05-15 18:38:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+#1 You can use below code to add phone number field in customer-registration liquid file. This code will help to display phone number field on the customer registration page. This will save phone number in note field.
+
+<input type="text" name="customer[note][phonenumber]">
+
+#2 You need to create a custom app to save phone number in customer phone number field. Please create the custom app from shopify store. 
+
+#3 You need to install PHP SHopifySDK on you PHP server.
+
+composer require phpclassic/php-shopify
+
+#4 Configure ShopifySDK
+
+$config = array(
+    'ShopUrl' => 'yourshop.myshopify.com',
+    'AccessToken' => '***ACCESS-TOKEN-FOR-THIRD-PARTY-APP***',
+);
+
+#5 Call file 'save-phone-number.php' on the customer create webhook.
+
+https://prnt.sc/C-gyGL_DTAgd
+
---
+++ save-phone-number.php
@@ -0,0 +1,49 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+/* install composer*/
+require_once './vendor/autoload.php';
+
+/*webhook verification function*/
+function verify_webhook($data, $hmac_header, $app_api_secret) {
+        $calculated_hmac = base64_encode(hash_hmac('sha256', $data, $app_api_secret, true));
+        return $calculated_hmac;    
+}
+
+// Set variable for Shopify webhook verification
+$webhookContent = '';
+$customerID = '';
+$countryCode = '';
+$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+$data = file_get_contents('php://input');
+$verified = verify_webhook($data, $hmac_header, "app_api_secret");
+$webhookContent = json_decode($data);
+/*Check webhook verification*/
+if(isset($verified) && !empty($webhookContent)){  
+
+    //Get Customer Detail
+    $customerId= $webhookContent->id;
+    $explodePhoneNum = explode("full_number:",$webhookContent->note);
+    $customerPhoneNumber = trim($explodePhoneNum[1]);
+
+    /*Using Shopify APi Update customer Phone Number*/
+    $config = array(
+            'ShopUrl' => 'store-testing.myshopify.com',
+            'AccessToken' => 'access_code'
+    );
+    $shopify = new PHPShopify\ShopifySDK($config);
+    $updatenote = array (
+            "note" => ''
+    );
+    $updatephone = array (
+            "phone" => $customerPhoneNumber
+    );
+    //update note field if phone number is valid
+    try {
+            $paramordesss = $shopify->Customer($customerId)->put($updatephone);                 
+            $paramordesss1 = $shopify->Customer($customerId)->put($updatenote);
+        } catch (ApiException $e) {
+                 echo $e->getMessage();
+        }
+
+}//check empty webhook data
+?>
